e8dc77f4c18263fd7f106d031d327c2c9b8f39bf
ELCOT <ELCOT@ADHI>
2021-06-06 23:12:02 +0530

    first commit made

---
+++ src/DataTypeExample/AllDataTypes.java
@@ -0,0 +1,19 @@
+package DataTypeExample;
+
+public class AllDataTypes {
+
+	public static void main (String[] args) {
+		short empId=103;
+		 String empName="pugazh";
+	     
+	     String empEmail="vijaytvpugazh.in";
+	     
+	     int empPhoneno=1234567890;
+	     int empSalary=88000;
+	     char empGender='M';
+	     String empCity="chennai";
+
+		System.out.println("employee id of " + empId + "-" + empName  + " \n can mail me @ " +
+		empEmail +"\n can reach me @ " + empPhoneno + " ,paid with " + empSalary + " being " + empGender + " from " + empCity);
+
+		         }}
